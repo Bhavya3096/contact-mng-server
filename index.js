ee6b91b6921841837baccf6617c92b3983cee395
Bhavya Sai Sree Mukkavilli <bhavya.mukkavilli@spaceinventive.com>
2026-06-18 11:22:20 +0530

    final cors fix

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ import {Router} from './routes/routes.js'
 const app=express()
 app.use(express.json())
 app.use(cors({
-    origin:["https://contact-mng-client-ybu4.vercel.app/login"],
+    origin:["https://contact-client-lioq4vqqs-m-bhavya-s-projects.vercel.app/"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
-}))
+}));
+app.options('*',cors());
 
 dotenv.config({path:"./config/.env"})
 
 app.use('/contactmng',Router)
-
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log("App is Running on port ${PORT}")
