8e9b23b80d9ed5fcacf64f4f5a8b5a9e75f0997a
Bhavya Sai Sree Mukkavilli <bhavya.mukkavilli@spaceinventive.com>
2026-06-18 12:29:24 +0530

    fix port1

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import './config/db.js'
-import cors from 'cors'
-import {Router} from './routes/routes.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { Router } from './routes/routes.js';
 
-const app=express()
-app.use(express.json())
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
 app.use(cors({
-    origin:["https://contact-client-lioq4vqqs-m-bhavya-s-projects.vercel.app/"],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true
+  origin: "https://contact-client-lioq4vqqs-m-bhavya-s-projects.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
 }));
-app.options('*',cors());
 
-dotenv.config({path:"./config/.env"})
+app.get('/', (req, res) => {
+  res.send('API running ');
+});
 
-app.use('/contactmng',Router)
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log("App is Running on port ${PORT}")
-})
+app.use('/contactmng', Router);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App is Running on port ${PORT}`);
+});
